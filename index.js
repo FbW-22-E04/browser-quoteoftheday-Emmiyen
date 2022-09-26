71bb8d9dcb10c3914c3c5e950bbab47e6b2d5590
Danica Yener <ny21037@googlemail.com>
2022-09-26 18:56:06 +0200

    quote of the day

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+// const quoteDisplay = document.querySelector('quoteDisplay')
+// const div = document.querySelector('div')
+// const h1 = document.querySelector('h1')
+// const button = document.querySelector('button')
+
+
+// quotes.value = newQuote()
+
+function newQuote() {
+  const randomNumber = Math.floor(Math.random() * (quotes.length))
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber]
+  
+
+}
+
 const quotes = [
   'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
   "Don't watch the clock; do what it does. Keep going.",
-  'The secret of getting ahead is getting started.',
-  'Well done is better than well said.',
-  'You miss 100% of the shots you don’t take.',
+   'The secret of getting ahead is getting started.',
+   'Well done is better than well said.',
+   'You miss 100% of the shots you don’t take.',
   'A goal is a dream with a deadline.',
   'Outstanding people have one thing in common: An absolute sense of mission.',
   'Trying is winning in the moment.',
@@ -12,3 +27,5 @@ const quotes = [
   'There is little success where there is little laughter.',
   'We cannot solve our problems with the same thinking we used when we created them.'
 ];
+
+
